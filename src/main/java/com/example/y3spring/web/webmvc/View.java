38f83ec232b42ddfa9992089b2cfde3040e543de
Kikikisum <2313810210@qq.com>
2023-08-09 00:29:19 +0800

    修复view中servlet无法导入的问题

--- src/main/java/com/example/y3spring/web/webmvc/View.java
+++ src/main/java/com/example/y3spring/web/webmvc/View.java
@@ -2,6 +2,8 @@ package com.example.y3spring.web.webmvc;
 
 
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.RandomAccessFile;
 import java.util.Map;
